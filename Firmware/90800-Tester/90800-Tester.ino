ca18aac785666ecbab14564ba4987acabf153ea5
ShoMisawa <shom419@gmail.com>
2023-08-28 07:15:54 -0500

    Adding toggle and lower the detection voltage from 15 to 10

--- Firmware/90800-Tester/90800-Tester.ino
+++ Firmware/90800-Tester/90800-Tester.ino
@@ -5,7 +5,7 @@
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>  //https://github.com/adafruit/Adafruit-GFX-Library
 #include <OneWire.h>
-//#include <DallasTemperature.h>   //https://github.com/milesburton/Arduino-Temperature-Control-Library
+
 #define ONE_WIRE_BUS 10
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -16,20 +16,17 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 Beastdevices_INA3221 ina3221(INA3221_ADDR41_VCC);
 
-//OneWire oneWire(ONE_WIRE_BUS);
-//DallasTemperature sensors(&oneWire);
-
-
 #define UI_BMPWIDTH  128
-//#define upperLimit 24*(1+0.05)
+
 #define upperLimit 24*(1+0.1)
 #define lowerLimit 24*(1-0.1)
 
-#define LED_8 8
-#define LED_9 9
-#define LED_10 10
-#define Relay 7
-#define Button 2
+#define LED_8                8
+#define LED_9                9
+#define LED_10               10
+#define Relay                7
+#define Button               2
+#define voltageDisplayToggle 3
 
 float R1 = 100000;
 float R2 = 10000;
@@ -64,17 +61,19 @@ int FLTestOK = 0;
 
 void setup() {
 
-  pinMode(Button, INPUT_PULLUP);
+  pinMode(Button, INPUT_PULLUP); // this line also has external pullup resistor
+  pinMode(voltageDisplayToggle, INPUT_PULLUP); // this line also has external pullup resistor
   pinMode(LED_8, OUTPUT);
   pinMode(LED_9, OUTPUT);
   pinMode(LED_10, OUTPUT);
-  //digitalWrite(3,HIGH);
   pinMode(Relay, OUTPUT);
+  
   digitalWrite(LED_8, LOW);
   digitalWrite(LED_9, LOW);
   digitalWrite(LED_10, LOW);
   digitalWrite(Relay, HIGH);
 
+  // SSD1306 Initialization...
   Serial.begin(9600);
   Serial.println("Starting up ...");
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -83,12 +82,9 @@ void setup() {
   display.setTextColor(SSD1306_WHITE);        // Draw white text
   display.display();
 
-  
-
 }
 
 void loop() {
-
 
   unsigned long currentMillis = millis();
 
@@ -97,8 +93,8 @@ void loop() {
   in_voltage = voltMeasure(0);
   in_voltage_2 = voltMeasure(1);
 
-  // if 24V line is greater than 15V, asume that the DUT is connected to the tester.  Which is indicated using voltageDetected variable.
-  if ((in_voltage > 15 || in_voltage_2 > 15) && voltageDetected == 0 && voltageDetected != 2) {
+  // if 24V line is greater than 10V, asume that the DUT is connected to the tester.  Which is indicated using voltageDetected variable.
+  if ((in_voltage > 10 || in_voltage_2 > 10) && voltageDetected == 0 && voltageDetected != 2) {
     voltageDetected = 1;
     state = 1; // Going to state 1
 
@@ -160,22 +156,8 @@ void loop() {
             voltageValidation_1 = 0;
             voltageValidation_2 = 0;
             digitalWrite(LED_9,LOW);
-          }
-
-          
+          } 
         }
-//        
-//        if ((in_voltage < upperLimit) && ((in_voltage > lowerLimit))) {
-//          voltageValidation_1 = 1;
-//        } else {
-//          voltageValidation_1 = 0;
-//        }
-//
-//        if ((in_voltage_2 < upperLimit) && ((in_voltage_2 > lowerLimit))) {
-//          voltageValidation_2 = 1;
-//        } else {
-//          voltageValidation_2 = 0;
-//        }
         
         if ( voltageValidation_1 == 1 && voltageValidation_2 == 1) {
           VoltageTestOK = 1;
@@ -190,6 +172,7 @@ void loop() {
           digitalWrite(LED_9, LOW);
         }
         break;
+        
         case 3: // Turning off relay for FL test
             digitalWrite(Relay,LOW);
             preDisplay(3, 0, 0);
@@ -200,6 +183,7 @@ void loop() {
             delay(1000);
             state = 4;
         break;
+        
         case 4: // Fire loop test
             
             preDisplay(2, 0, 0);
@@ -208,8 +192,6 @@ void loop() {
             display.display();
             
         if (relayActiveFlag == 1){
-          
-          
           // Resetting the voltageValidation for fire loop detection
           voltageValidation_1 = 0;
           voltageValidation_2 = 0;
@@ -241,17 +223,19 @@ void loop() {
             delay(1000);
             digitalWrite(LED_10, LOW);
             state = 5;
-          }
-          
+          } 
         }
-
         break;
+        
         case 5:
-//          preDisplay(3, 0, 0);
-//          display.println("   Case 5");
-//          //display.println("   ");
-//          display.display();
-//          delay(5000);
+
+          // while toggle switch is in the LOW state, display the output voltage reading.
+          while (digitalRead(voltageDisplayToggle) == LOW){
+            preDisplay(2, 0, 0);
+            printDisplay("J6", in_voltage);
+            printDisplay("J7", in_voltage_2);
+            display.display();
+          }
           
           if (VoltageTestOK == 1 && FLTestOK == 1 && powerDetectionOK == 1){
             preDisplay(3, 0, 0);

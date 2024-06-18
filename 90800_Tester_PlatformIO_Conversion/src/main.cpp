78af65328944d46ebdae29d8e3d453022fe11808
Sho Misawa <41084688+ShoMisawa@users.noreply.github.com>
2024-06-17 21:39:29 -0500

    Add files via upload

--- 90800_Tester_PlatformIO_Conversion/src/main.cpp
+++ 90800_Tester_PlatformIO_Conversion/src/main.cpp
@@ -1,10 +1,17 @@
 /*
 
+Date: 06/17/2024
+
 Program Written for Seeeduino-XIAO
 Microcontroller: ATSAMD21G18A
 Datasheet: https://files.seeedstudio.com/wiki/Seeeduino-XIAO/res/ATSAMD21G18A-MU-Datasheet.pdf
 
-
+Libraries:
+ - U8g2 by oliver (required to download)
+ - Adafruit SleepyDog Library by Adafruit (required to download)
+ - graphics.h is local library that consist of graphics data
+ - global_variables.h consist of variables
+  
 
 */
 
@@ -12,19 +19,24 @@ Datasheet: https://files.seeedstudio.com/wiki/Seeeduino-XIAO/res/ATSAMD21G18A-MU
 #include <U8g2lib.h>
 #include <Wire.h>
 #include <SPI.h>
+#include <Adafruit_SleepyDog.h>
 #include "graphics.h"
 #include "global_variables.h"
-#include <Adafruit_SleepyDog.h>
 
 /*U8G2 Initialization*/
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // initialization for the used OLED display
 /*------------------------------------------------------------------------------------------------------------------------*/
 
+/* Constants */
+const float SCHOTTKY_VOLTAGE_DROP = 0.3;
+
 /* Function Definition */
 void resetParameters();
 float voltMeasure(int channel);
+void updateDisplay();
+void displayVoltageReading(float voltage, float voltage_2);
 
-
+/* Setup Function */
 void setup() {
 
   pinMode(Button, INPUT_PULLUP); // this line also has external pullup resistor
@@ -51,7 +63,7 @@ void setup() {
   u8g2.sendBuffer();
   delay(1000); // One second splash screen
 
-  analogReadResolution(12);
+  analogReadResolution(12); // Setting ADC Resolution for Pin 12
 
   // Setup the watchdog timer
   int wdtTimeout = Watchdog.enable(8000); // Enable WDT with 8 seconds timeout
@@ -70,16 +82,17 @@ void loop() {
     digitalWrite(LED_9, HIGH);
     digitalWrite(LED_10, HIGH);
   }else{
+    in_voltage = voltMeasure(0);
+    in_voltage_2 =  voltMeasure(1);
+  // // J2 on the tester PWA
+  // adc_value = analogRead(0);
+  // adc_voltage = (adc_value * ref_voltage) / 4095.0;
+  // in_voltage = (adc_voltage / resRatio) + 0.3; // 0.3 is for schottky diode voltage adjustment
 
-  // J2 on the tester PWA
-  adc_value = analogRead(0);
-  adc_voltage = (adc_value * ref_voltage) / 4095.0;
-  in_voltage = (adc_voltage / resRatio) + 0.3; // 0.3 is for schottky diode voltage adjustment
-
-  // J4 on the tester PWA
-  adc_value_2 = analogRead(1);
-  adc_voltage_2 = (adc_value_2 * ref_voltage) / 4095.0;
-  in_voltage_2 = (adc_voltage_2 / resRatio) + 0.3; // 0.3 is for schottky diode voltage adjustment
+  // // J4 on the tester PWA
+  // adc_value_2 = analogRead(1);
+  // adc_voltage_2 = (adc_value_2 * ref_voltage) / 4095.0;
+  // in_voltage_2 = (adc_voltage_2 / resRatio) + 0.3; // 0.3 is for schottky diode voltage adjustment
 
   switch(state){
     case 0: // Waiting for 24V to come in... Upon detection move to the next case
@@ -93,15 +106,18 @@ void loop() {
           iconVisible = !iconVisible;
         }
         
-        // J2 on the tester PWA
-        adc_value = analogRead(0);
-        adc_voltage = (adc_value * ref_voltage) / 4095.0;
-        in_voltage = (adc_voltage / resRatio) + 0.3; // 0.3 is for schottky diode voltage adjustment
+        in_voltage = voltMeasure(0);
+        in_voltage_2 = voltMeasure(1);
 
-        // J4 on the tester PWA
-        adc_value_2 = analogRead(1);
-        adc_voltage_2 = (adc_value_2 * ref_voltage) / 4095.0;
-        in_voltage_2 = (adc_voltage_2 / resRatio) + 0.3; // 0.3 is for schottky diode voltage adjustment
+        // // J2 on the tester PWA
+        // adc_value = analogRead(0);
+        // adc_voltage = (adc_value * ref_voltage) / 4095.0;
+        // in_voltage = (adc_voltage / resRatio) + 0.3; // 0.3 is for schottky diode voltage adjustment
+
+        // // J4 on the tester PWA
+        // adc_value_2 = analogRead(1);
+        // adc_voltage_2 = (adc_value_2 * ref_voltage) / 4095.0;
+        // in_voltage_2 = (adc_voltage_2 / resRatio) + 0.3; // 0.3 is for schottky diode voltage adjustment
         
         u8g2.clearBuffer();
         u8g2.setBitmapMode(1);
@@ -116,12 +132,10 @@ void loop() {
 
       } 
 
-      delay(1000);
-      
-      voltageDetected = 1; // after coming out from the while loop, the voltage should be greater than 15VDC
-      state = 1;
-      counterGraphics = 0;
-
+    delay(1000);
+    voltageDetected = 1; // after coming out from the while loop, the voltage should be greater than 15VDC
+    state = 1;
+    counterGraphics = 0;
     break;
 
     case 1: // Power detected... Transition into voltage confirmation
@@ -135,42 +149,46 @@ void loop() {
         state = 0;
         digitalWrite(LED_8, LOW);
       }
-      
     break;
 
     case 2: // Showing voltage reading on OLED and waiting for confirmation button to be pressed
       // Waiting for the confirmation by pressing the button located on the right hand bottom side
       while(!digitalRead(Button) == LOW){
-          adc_value = analogRead(0);
-          adc_voltage = (adc_value * ref_voltage) / 4095.0;
-          in_voltage = (adc_voltage / resRatio) + 0.3; // 0.3 is for schottky diode voltage adjustment
+          in_voltage = voltMeasure(0);
+          in_voltage_2 = voltMeasure(1);
+          
+          // adc_value = analogRead(0);
+          // adc_voltage = (adc_value * ref_voltage) / 4095.0;
+          // in_voltage = (adc_voltage / resRatio) + 0.3; // 0.3 is for schottky diode voltage adjustment
 
-          // J4 on the tester PWA
-          adc_value_2 = analogRead(1);
-          adc_voltage_2 = (adc_value_2 * ref_voltage) / 4095.0;
-          in_voltage_2 = (adc_voltage_2 / resRatio) + 0.3; // 0.3 is for schottky diode voltage adjustment
+          // // J4 on the tester PWA
+          // adc_value_2 = analogRead(1);
+          // adc_voltage_2 = (adc_value_2 * ref_voltage) / 4095.0;
+          // in_voltage_2 = (adc_voltage_2 / resRatio) + 0.3; // 0.3 is for schottky diode voltage adjustment
 
-          u8g2.setBitmapMode(1);
-          u8g2.clearBuffer(); 
-          u8g2.drawFrame(2, 2, 124, 62);
-          u8g2.drawLine(3, 15, 125, 15);
-          u8g2.setFont(u8g2_font_9x15B_mr);
-          //u8g2.drawStr(27, 30, "J6:");
-          u8g2.setFont(u8g2_font_9x15B_mr);
-          sprintf(voltagebuffer,"J6: %.2f", in_voltage);
-          u8g2.drawStr(10, 30, voltagebuffer);
-          u8g2.setFont(u8g2_font_9x15B_mr);
-          u8g2.drawStr(83, 30, "V");
-          u8g2.setFont(u8g2_font_haxrcorp4089_tr);
-          u8g2.drawStr(30, 13, "Voltage Reading");
-          u8g2.setFont(u8g2_font_9x15B_mr);
-          //u8g2.drawStr(27, 45, "J7:");
-          u8g2.setFont(u8g2_font_9x15B_mr);
-          sprintf(voltagebuffer, "J7: %.2f", in_voltage_2);
-          u8g2.drawStr(10, 45, voltagebuffer);
-          u8g2.setFont(u8g2_font_9x15B_mr);
-          u8g2.drawStr(83, 45, "V");
-          u8g2.sendBuffer();
+          updateDisplay();
+
+          // u8g2.setBitmapMode(1);
+          // u8g2.clearBuffer(); 
+          // u8g2.drawFrame(2, 2, 124, 62);
+          // u8g2.drawLine(3, 15, 125, 15);
+          // u8g2.setFont(u8g2_font_9x15B_mr);
+          // //u8g2.drawStr(27, 30, "J6:");
+          // u8g2.setFont(u8g2_font_9x15B_mr);
+          // sprintf(voltagebuffer,"J6: %.2f", in_voltage);
+          // u8g2.drawStr(10, 30, voltagebuffer);
+          // u8g2.setFont(u8g2_font_9x15B_mr);
+          // u8g2.drawStr(83, 30, "V");
+          // u8g2.setFont(u8g2_font_haxrcorp4089_tr);
+          // u8g2.drawStr(30, 13, "Voltage Reading");
+          // u8g2.setFont(u8g2_font_9x15B_mr);
+          // //u8g2.drawStr(27, 45, "J7:");
+          // u8g2.setFont(u8g2_font_9x15B_mr);
+          // sprintf(voltagebuffer, "J7: %.2f", in_voltage_2);
+          // u8g2.drawStr(10, 45, voltagebuffer);
+          // u8g2.setFont(u8g2_font_9x15B_mr);
+          // u8g2.drawStr(83, 45, "V");
+          // u8g2.sendBuffer();
 
           if (in_voltage < upperLimit && in_voltage > lowerLimit) {
             voltageValidation_1 = 1;
@@ -195,7 +213,6 @@ void loop() {
     case 3: // Turning off relay for FL test
             u8g2.setBitmapMode(1);
             u8g2.clearBuffer(); 
-        
             u8g2.drawFrame(2, 2, 125, 61);
             u8g2.drawLine(2, 14, 126, 14);
             u8g2.setFont(u8g2_font_haxrcorp4089_tr);
@@ -394,7 +411,7 @@ void resetParameters(){
   
   state = 0;
   
-  previousMillis;
+  // previousMillis;
   count = 1; // for development purpose, it is set to the state
   
   relayActiveFlag = 0;
@@ -411,22 +428,64 @@ void resetParameters(){
 float voltMeasure(int channel) {
   analogReadResolution(12);
   
-  if(channel == 0){
-    
-    adc_value = analogRead(channel);
-    adc_voltage = (adc_value * ref_voltage) / 4095.0;
-    in_voltage = adc_voltage / resRatio;
-    return in_voltage;
-    
-  }
+  float adc_voltage = (analogRead(channel) * ref_voltage) / 4095.0;
+  return (adc_voltage / resRatio) + SCHOTTKY_VOLTAGE_DROP;
 
-  if(channel == 1){
+  // if(channel == 0){
     
-    adc_value_2 = analogRead(channel);
-    adc_voltage_2 = (adc_value_2 * ref_voltage) / 4095.0;
-    in_voltage_2 = adc_voltage_2 / resRatio;
-    return in_voltage_2;
+  //   adc_value = analogRead(channel);
+  //   adc_voltage = (adc_value * ref_voltage) / 4095.0;
+  //   in_voltage = adc_voltage / resRatio;
+  //   return in_voltage;
+    
+  // }
 
-  }
+  // if(channel == 1){
+    
+  //   adc_value_2 = analogRead(channel);
+  //   adc_voltage_2 = (adc_value_2 * ref_voltage) / 4095.0;
+  //   in_voltage_2 = adc_voltage_2 / resRatio;
+  //   return in_voltage_2;
+
+  // }
   
 } // end of voltMeter function
+
+/* Function to update display */
+void updateDisplay() {
+  u8g2.setBitmapMode(1);
+  u8g2.clearBuffer();
+  u8g2.drawFrame(2, 2, 124, 62);
+  u8g2.drawLine(3, 15, 125, 15);
+  u8g2.setFont(u8g2_font_9x15B_mr);
+  sprintf(voltagebuffer, "J6: %.2f", in_voltage);
+  u8g2.drawStr(10, 30, voltagebuffer);
+  u8g2.drawStr(83, 30, "V");
+  sprintf(voltagebuffer, "J7: %.2f", in_voltage_2);
+  u8g2.drawStr(10, 45, voltagebuffer);
+  u8g2.drawStr(83, 45, "V");
+  u8g2.sendBuffer();
+}
+
+/* Function to display voltage reading */
+void displayVoltageReading(float voltage, float voltage_2) {
+  u8g2.setBitmapMode(1);
+  u8g2.clearBuffer();
+  u8g2.drawFrame(2, 2, 124, 62);
+  u8g2.drawLine(3, 15, 125, 15);
+  u8g2.setFont(u8g2_font_haxrcorp4089_tr);
+  sprintf(voltagebuffer, "J6: %.2f", voltage);
+  u8g2.drawStr(27, 30, voltagebuffer);
+  u8g2.drawStr(83, 30, "V");
+  sprintf(voltagebuffer, "J7: %.2f", voltage_2);
+  u8g2.drawStr(27, 45, voltagebuffer);
+  u8g2.drawStr(83, 45, "V");
+  if (voltage < upperLimit && voltage > lowerLimit) {
+    u8g2.drawXBMP(99, 38, 24, 24, image_icons8_outline_effect_tickmark_under_a_square_box_24_bits);
+  } else {
+    u8g2.setDrawColor(0);
+    u8g2.drawBox(97, 36, 27, 26);
+    u8g2.setDrawColor(1);
+  }
+  u8g2.sendBuffer();
+}
